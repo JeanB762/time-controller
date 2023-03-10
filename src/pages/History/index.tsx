ae6a60e4a2216124c74252684fd7bf388267c257
Jean Borges <jeanborges946@gmail.com>
2023-03-10 18:02:13 -0300

    feature: Listagem do histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,48 @@
+import { HistoryContainer, HistoryList } from './styles';
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutos</td>
+              <td>Há 1 semana</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutos</td>
+              <td>Há 1 semana</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutos</td>
+              <td>Há 1 semana</td>
+              <td>Concluída</td>
+            </tr>
+            <tr>
+              <td>Tarefa 01</td>
+              <td>20 minutos</td>
+              <td>Há 1 semana</td>
+              <td>Concluída</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
