1be5f8f423a126797aaef9eaaab5630bd6e8290d
Jean Borges <jeanborges946@gmail.com>
2023-03-10 12:13:28 -0300

    feature: Criação do layout das páginas, do componente de header e inicio da estilização da Home

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { HeaderContainer } from './styles';
+import { Timer, Scroll } from 'phosphor-react';
+import { NavLink } from 'react-router-dom';
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <NavLink to='/' title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to='/history' title='Histórico'>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,37 @@
+import { Play } from 'phosphor-react';
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles';
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action=''>
+        <FormContainer>
+          <label htmlFor='task'>Vou trabalhar em: </label>
+          <input id='task' />
+          <label htmlFor='minutesAmmount'>Durante</label>
+          <input type='number' id='minutesAmmount' />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type='submit'>
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  );
+}
