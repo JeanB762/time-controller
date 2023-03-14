f1fbbe7814abd7342a057c64e76988d4027f1814
Jean Borges <jeanborges946@gmail.com>
2023-03-14 10:54:23 -0300

    feature: Iniciando a validação dos campos do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import zod from 'zod';
+
 import {
   CountdownContainer,
   FormContainer,
@@ -10,8 +13,18 @@ import {
   TaskInput,
 } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmmount: zod
+    .number()
+    .min(5, 'O ciclo deve ser mínimo 5 minutos')
+    .max(60, 'O ciclo deve ser no máximo 60 minutos'),
+});
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
 
   function handleCreateNewCycle(data: any) {
     console.log(data);
