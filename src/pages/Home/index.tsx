f5a737a23fa319f3c72b5a1c8ce4cb74916ec820
Jean Borges <jeanborges946@gmail.com>
2023-03-15 14:44:54 -0300

    feature: Adicionando o timer ao título da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -88,6 +88,10 @@ export function Home() {
   const minutes = String(minutesAmmount).padStart(2, '0');
   const seconds = String(secondsAmmount).padStart(2, '0');
 
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${seconds}`;
+  }, [minutes, seconds, activeCycle]);
+
   const task = watch('task');
   const isSubmitDisabled = !task;
 
